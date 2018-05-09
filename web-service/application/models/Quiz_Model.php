82dc6e2fc46ab93d62adf5704fb36865e5d01241
58160638 <58160638@go.buu.ac.th>
2018-05-09 15:38:10 +0700

    Edit models

--- web-service/application/models/Quiz_Model.php
+++ web-service/application/models/Quiz_Model.php
@@ -1,39 +1,5 @@
 <?php
 class Quiz_Model extends CI_Model {
 	
-	public function list_all_quiz(){
-		$result = $this->db->get('quiz');
-		return $result->result_array();
-	}
-
-	public function get_quiz_by_id($id){
-		$this->db->where('quiz_id',$id);
-		$result = $this->db->get('list_exam');
-		return $result->result_array();
-	}
-
-	public function random_exam($quiz_id){
-		//echo $quiz_id;
-		$numbers = range(1, 20);
-		shuffle($numbers);
-		//print_r ($numbers);
-		for($i = 0 ; $i < 10 ; $i++){
-			$number[] = $numbers[$i];
-		}
-
-		$where = "quiz_id = '$quiz_id'
-		AND (exam_number = '$number[0]' OR exam_number = '$number[1]' OR exam_number = '$number[2]' 
-		OR exam_number = '$number[3]' OR exam_number = '$number[4]' OR exam_number = '$number[5]' 
-		OR exam_number = '$number[6]' OR exam_number = '$number[7]' OR exam_number = '$number[8]' 
-		OR exam_number = '$number[9]')";
-
-		$wheres = "quiz_id = '$quiz_id'
-		AND exam_number = '1' ";
-
-		$this->db->where($wheres);
-		$result = $this->db->get('list_exam');
-		return $result->result_array();
-	}
-
 
 }
